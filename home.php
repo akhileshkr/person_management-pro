68078772a5aaa409103d734c4e743795c0c5296d
akhileshkr <akhilesh0614@gmail.com>
2017-01-26 19:01:06 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="frm" action="action/home_action.php" method="post">
+        <input type="submit" name="registration" value="registration"><br>
+        <input type="submit" name="cancel" value="cancel">   
+        <a href=""> view</a>
+        </form>
+    </body>
+</html>
